375ceb3a72604574071751c39f7268c85039a588
Max Soe <maxsoe@gmail.com>
2023-01-06 14:04:23 +0400

    8-70 Step 5 - Randomise it

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -28,13 +29,17 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: TextButton(
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
         onPressed: () {
-          debugPrint('Ball pressed');
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+          debugPrint('Ball pressed - $ballNumber');
         },
       ),
     );
